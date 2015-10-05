7be32ae657c019d2f2fb4c3863e6399c6fb6c09a
Artem Kuznetsov <tema.kuz@icloud.com>
2015-10-05 19:13:18 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Артем Кузнецов on 05.10.15.
+//  Copyright © 2015 Артём Кузнецов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
